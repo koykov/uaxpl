b24977bf27cc423cc7d28fb48ae46b39c43f3d55
Konstantin Voykov <koykov0@gmail.com>
2022-04-23 00:58:57 +0300

    uavectorc: parse client sources.

---
+++ cmd/uavectorc/go.mod
@@ -0,0 +1,5 @@
+module github.com/koykov/uavector/cmd/uavectorc
+
+go 1.16
+
+require gopkg.in/yaml.v2 v2.4.0
--- cmd/uavectorc/mod_client.go
+++ cmd/uavectorc/mod_client.go
@@ -1,6 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"path/filepath"
+
+	"gopkg.in/yaml.v2"
+)
+
+type clientTuple struct {
+	Regex   string        `yaml:"regex"`
+	Name    string        `yaml:"name"`
+	Version string        `yaml:"version,omitempty"`
+	Engine  *ClientEngine `yaml:"engine,omitempty"`
+	URL     string        `yaml:"url,omitempty"`
+	Type    string        `yaml:"type,omitempty"`
+}
+
+type ClientEngine struct {
+	Default  string            `yaml:"default"`
+	Versions map[string]string `yaml:"versions,omitempty"`
+}
 
 type clientModule struct{}
 
@@ -12,5 +33,32 @@ func (m clientModule) Validate(input, _ string) error {
 }
 
 func (m clientModule) Compile(w moduleWriter, input, target string) (err error) {
-	return
+	if len(target) == 0 {
+		target = "client_repo.go"
+	}
+	files, err := filepath.Glob(input + "/*.yml")
+	if err != nil {
+		return err
+	}
+	for i := 0; i < len(files); i++ {
+		var body []byte
+		if body, err = ioutil.ReadFile(files[i]); err != nil {
+			return
+		}
+		if len(body) == 0 {
+			err = errors.New("nothing to parse")
+			return
+		}
+
+		var tuples []clientTuple
+		if err = yaml.Unmarshal(body, &tuples); err != nil {
+			return
+		}
+
+		for j := 0; j < len(tuples); j++ {
+			fmt.Println(tuples[j].Regex)
+		}
+	}
+
+	return nil
 }
