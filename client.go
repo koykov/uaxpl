74597d0032b72824e59161b920f095c0b60d18c6
Konstantin Voykov <koykov0@gmail.com>
2022-04-23 22:37:46 +0300

    Client repo struct.

--- client.go
+++ client.go
@@ -16,3 +16,15 @@ const (
 	ClientParserAll = ClientParserFeedReader | ClientParserMobileApp | ClientParserMediaPlayer | ClientParserPIM |
 		ClientParserBrowser | ClientParserLibrary
 )
+
+type engFn func(string, entry.Entry64) entry.Entry64
+
+type cr struct {
+	re int32         // regex index
+	si entry.Entry64 // string index
+	vi int8          // version index
+	ed entry.Entry64 // default engine
+	ef int32         // engine func index
+	ul entry.Entry64 // url
+	tp entry.Entry64 // type string
+}
