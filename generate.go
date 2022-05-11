18f05663fc23af0f131ce5998d3f91ea13509da1
Konstantin Voykov <koykov0@gmail.com>
2022-05-12 01:38:10 +0300

    Add generate file.

---
+++ generate.go
@@ -0,0 +1,6 @@
+package uaxpl
+
+//go:generate uaxplc -module=client -input=~/dev/php/device-detector/regexes/client
+//go:generate uaxplc -module=client -input=~/dev/php/device-detector/regexes/client
+//go:generate uaxplc -module=hint -input=~/dev/php/device-detector/regexes/client/hints
+//go:generate uaxplc -module=os -input=~/dev/php/device-detector/regexes/oss.yml
