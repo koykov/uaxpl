5b56089345adcb7c0f5bc77b1e822c4e0a418883
Konstantin Voykov <koykov0@gmail.com>
2022-08-01 00:43:53 +0300

    Custom test issue: check string equality case-insensitive.

--- custom_test.go
+++ custom_test.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"strings"
 	"testing"
 )
 
@@ -27,6 +28,10 @@ type customDS struct {
 }
 
 func TestCustomParse(t *testing.T) {
+	scmp := func(a, b string) bool {
+		return strings.ToLower(a) == strings.ToLower(b)
+	}
+
 	testDS := func(filename string) error {
 		var ds []customDS
 		contents, err := os.ReadFile(filename)
@@ -42,35 +47,35 @@ func TestCustomParse(t *testing.T) {
 			stage := &ds[i]
 			buf = buf[:0]
 			ctx := AcquireWithSrcStr(stage.UA)
-			if ct := ctx.GetClientType(); len(stage.Client.Type) > 0 && ct.String() != stage.Client.Type {
+			if ct := ctx.GetClientType(); len(stage.Client.Type) > 0 && !scmp(ct.String(), stage.Client.Type) {
 				buf = append(buf, fmt.Errorf("client type mismatch: need '%s' got '%s'", stage.Client.Type, ct))
 			}
-			if b := ctx.GetBrowser(); len(stage.Client.Name) > 0 && b != stage.Client.Name {
+			if b := ctx.GetBrowser(); len(stage.Client.Name) > 0 && !scmp(b, stage.Client.Name) {
 				buf = append(buf, fmt.Errorf("browser mismatch: need '%s' got '%s'", stage.Client.Name, b))
 			}
-			if bv := ctx.GetBrowserVersionString(); len(stage.Client.Version) > 0 && bv != stage.Client.Version {
+			if bv := ctx.GetBrowserVersionString(); len(stage.Client.Version) > 0 && !scmp(bv, stage.Client.Version) {
 				buf = append(buf, fmt.Errorf("browser version mismatch: need '%s' got '%s'", stage.Client.Version, bv))
 			}
-			if e := ctx.GetEngine(); len(stage.Client.Engine) > 0 && e != stage.Client.Engine {
+			if e := ctx.GetEngine(); len(stage.Client.Engine) > 0 && !scmp(e, stage.Client.Engine) {
 				buf = append(buf, fmt.Errorf("engine mismatch: need '%s' got '%s'", stage.Client.Engine, e))
 			}
-			if ev := ctx.GetEngineVersionString(); len(stage.Client.EngineVersion) > 0 && ev != stage.Client.EngineVersion {
+			if ev := ctx.GetEngineVersionString(); len(stage.Client.EngineVersion) > 0 && !scmp(ev, stage.Client.EngineVersion) {
 				buf = append(buf, fmt.Errorf("engine version mismatch: need '%s' got '%s'", stage.Client.EngineVersion, ev))
 			}
 
-			if dt := ctx.GetDeviceType(); len(stage.Device.Type) > 0 && dt.String() != stage.Device.Type {
+			if dt := ctx.GetDeviceType(); len(stage.Device.Type) > 0 && !scmp(dt.String(), stage.Device.Type) {
 				buf = append(buf, fmt.Errorf("device type mismatch: need '%s' got '%s'", stage.Device.Type, dt))
 			}
-			if b := ctx.GetBrand(); len(stage.Device.Brand) > 0 && b != stage.Device.Brand {
+			if b := ctx.GetBrand(); len(stage.Device.Brand) > 0 && !scmp(b, stage.Device.Brand) {
 				buf = append(buf, fmt.Errorf("brand mismatch: need '%s' got '%s'", stage.Device.Brand, b))
 			}
-			if m := ctx.GetModel(); len(stage.Device.Model) > 0 && m != stage.Device.Model {
+			if m := ctx.GetModel(); len(stage.Device.Model) > 0 && !scmp(m, stage.Device.Model) {
 				buf = append(buf, fmt.Errorf("model mismatch: need '%s' got '%s'", stage.Device.Model, m))
 			}
-			if o := ctx.GetOS(); len(stage.Device.OS) > 0 && o != stage.Device.OS {
+			if o := ctx.GetOS(); len(stage.Device.OS) > 0 && !scmp(o, stage.Device.OS) {
 				buf = append(buf, fmt.Errorf("OS mismatch: need '%s' got '%s'", stage.Device.OS, o))
 			}
-			if ov := ctx.GetOSVersionString(); len(stage.Device.OSVersion) > 0 && ov != stage.Device.OSVersion {
+			if ov := ctx.GetOSVersionString(); len(stage.Device.OSVersion) > 0 && !scmp(ov, stage.Device.OSVersion) {
 				buf = append(buf, fmt.Errorf("OS version mismatch: need '%s' got '%s'", stage.Device.OSVersion, ov))
 			}
 			Release(ctx)
