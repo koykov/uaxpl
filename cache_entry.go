fd554e470c0be8d915c5063bae6985a51c170087
Konstantin Voykov <koykov0@gmail.com>
2026-07-14 18:42:01 +0300

    cache: marshal/unmarshal entry issues

--- cache_entry.go
+++ cache_entry.go
@@ -1,6 +1,9 @@
 package uaxpl
 
 import (
+	"encoding/binary"
+	"io"
+
 	"github.com/koykov/bitset"
 	"github.com/koykov/bytealg"
 	"github.com/koykov/entry"
@@ -42,4 +45,115 @@ func (r *CacheEntry) FromCtx(ctx *Ctx) {
 	r.OSName64 = ctx.osName64
 	r.OSVersion64 = ctx.osVersion64
 	r.Data = bytealg.Copy(ctx.buf)
+}
+
+func (r *CacheEntry) Size() int {
+	return r.minsz() + len(r.Data)
+}
+
+func (r *CacheEntry) minsz() int {
+	return 8 + // Bitset
+		1 + // ClientType
+		8 + // ClientName64
+		8 + // ClientVersion64
+		8 + // EngineName64
+		8 + // EngineVersion64
+		2 + // DeviceType
+		8 + // BrandName64
+		8 + // ModelName64
+		8 + // OSName64
+		8 + // OSVersion64
+		8 + // Hkey
+		8 + // Timestamp
+		4 // len(Data)
+}
+
+func (r *CacheEntry) MarshalTo(buf []byte) (int, error) {
+	if len(buf) < r.Size() {
+		return 0, io.ErrShortBuffer
+	}
+	var off int
+
+	binary.LittleEndian.PutUint64(buf[off:off+8], uint64(r.Bitset))
+	off += 8
+
+	buf[off] = byte(r.ClientType)
+	off++
+	binary.LittleEndian.PutUint64(buf[off:off+8], uint64(r.ClientName64))
+	off += 8
+	binary.LittleEndian.PutUint64(buf[off:off+8], uint64(r.ClientVersion64))
+	off += 8
+	binary.LittleEndian.PutUint64(buf[off:off+8], uint64(r.EngineName64))
+	off += 8
+	binary.LittleEndian.PutUint64(buf[off:off+8], uint64(r.EngineVersion64))
+	off += 8
+
+	binary.LittleEndian.PutUint16(buf[off:off+2], uint16(r.DeviceType))
+	off += 2
+	binary.LittleEndian.PutUint64(buf[off:off+8], uint64(r.BrandName64))
+	off += 8
+	binary.LittleEndian.PutUint64(buf[off:off+8], uint64(r.ModelName64))
+	off += 8
+	binary.LittleEndian.PutUint64(buf[off:off+8], uint64(r.OSName64))
+	off += 8
+	binary.LittleEndian.PutUint64(buf[off:off+8], uint64(r.OSVersion64))
+	off += 8
+
+	binary.LittleEndian.PutUint64(buf[off:off+8], r.Hkey)
+	off += 8
+	binary.LittleEndian.PutUint64(buf[off:off+8], uint64(r.Timestamp))
+	off += 8
+
+	binary.LittleEndian.PutUint32(buf[off:off+4], uint32(len(r.Data)))
+	off += 4
+	copy(buf[off:], r.Data)
+	off += len(r.Data)
+
+	return off, nil
+}
+
+func (r *CacheEntry) Unmarshal(data []byte) error {
+	if len(data) < r.minsz() {
+		return io.ErrUnexpectedEOF
+	}
+
+	var off int
+	r.Bitset = bitset.Bitset(binary.LittleEndian.Uint64(data[off : off+8]))
+	off += 8
+
+	r.ClientType = ClientType(data[off])
+	off++
+	r.ClientName64 = entry.Entry64(binary.LittleEndian.Uint64(data[off : off+8]))
+	off += 8
+	r.ClientVersion64 = entry.Entry64(binary.LittleEndian.Uint64(data[off : off+8]))
+	off += 8
+	r.EngineName64 = entry.Entry64(binary.LittleEndian.Uint64(data[off : off+8]))
+	off += 8
+	r.EngineVersion64 = entry.Entry64(binary.LittleEndian.Uint64(data[off : off+8]))
+	off += 8
+
+	r.DeviceType = DeviceType(binary.LittleEndian.Uint16(data[off : off+2]))
+	off += 2
+	r.BrandName64 = entry.Entry64(binary.LittleEndian.Uint64(data[off : off+8]))
+	off += 8
+	r.ModelName64 = entry.Entry64(binary.LittleEndian.Uint64(data[off : off+8]))
+	off += 8
+	r.OSName64 = entry.Entry64(binary.LittleEndian.Uint64(data[off : off+8]))
+	off += 8
+	r.OSVersion64 = entry.Entry64(binary.LittleEndian.Uint64(data[off : off+8]))
+	off += 8
+
+	r.Hkey = binary.LittleEndian.Uint64(data[off : off+8])
+	off += 8
+	r.Timestamp = int64(binary.LittleEndian.Uint64(data[off : off+8]))
+	off += 8
+
+	ln := binary.LittleEndian.Uint32(data[off : off+4])
+	off += 4
+	if len(data) < off+int(ln) {
+		return io.ErrUnexpectedEOF
+	}
+	r.Data = append(r.Data[:0], data[off:off+int(ln)]...)
+
+	return nil
 }
--- go.mod
+++ go.mod
@@ -8,6 +8,12 @@ require (
 	github.com/koykov/byteconv v1.0.1
 	github.com/koykov/entry v1.0.2
 	github.com/koykov/hash v1.0.5
+	github.com/stretchr/testify v1.11.1
 )
 
-require github.com/koykov/byteseq v1.0.1 // indirect
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/koykov/byteseq v1.0.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
